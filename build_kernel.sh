3dfd7b7c3026aad111d96de8281c6561d1f0aeb7
RAFS SNAKE <herombr2023@gmail.com>
2024-10-04 00:01:33 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
-make ARCH=arm64 RafsSNK-a12snsxx_defconfig
+make ARCH=arm64 RafsSnake-a12snsxx_defconfig
 make ARCH=arm64 -j64
